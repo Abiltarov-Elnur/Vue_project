6935ca837cbdd62f40704dbc45be9515983536d9
Elnur Abiltarov <Asus@DESKTOP-RV00K7V>
2025-06-10 09:57:17 +0300

    page_9.2

--- src/views/Ads/AdListView.vue
+++ src/views/Ads/AdListView.vue
@@ -1,18 +1,63 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'AdListView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    <v-container>
+       <h1 class="text--secondary mb-3 mt-3">My ads</h1>
+       <v-card v-for="ad in ads" :key="ad.id" class="mx-auto" max-width="1000">
+           <v-row>
+               <v-img :src="ad.src" height="175px"></v-img>
+               <v-spacer></v-spacer>
+           </v-row>
+           <v-row>
+               <v-card-text>
+                   <h2 class="text--primary">{{ ad.title }}</h2>
+                   <p>{{ ad.desc }}</p>
+               </v-card-text>
+               <v-card-actions>
+                   <v-spacer></v-spacer>
+                   <v-btn color="primary" variant="text" :to="'/ad/' + ad.id">
+                       Open
+                   </v-btn>
+                   <v-spacer></v-spacer>
+               </v-card-actions>
+           </v-row>
+       </v-card>
+   </v-container>
+ </template>
+ 
+ <script>
+ export default {
+   data() {
+       return {
+        ads: [
+               {
+                   title: "First",
+                   desc: "First Desc",
+                   promo: true,
+                   src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+                   id: "1",
+               },
+               {
+                   title: "Second",
+                   desc: "Second Desc",
+                   promo: true,
+                   src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+                   id: "2",
+               },
+               {
+                   title: "Third",
+                   desc: "Thitd Desc",
+                   promo: true,
+                   src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+                   id: "3",
+               },
+               {
+                   title: "Fouth",
+                   desc: "Fouth Desc",
+                   promo: true,
+                   src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+                   id: "4",
+               },
+           ],
+       };
+   },
+ };
+ </script>
--- src/views/Ads/AdView.vue
+++ src/views/Ads/AdView.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
-</template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'AdView',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+  <v-container>
+    <v-row>
+      <v-col cols="12">
+        <v-card class="mt-5">
+          <v-img height="300px" src="https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg"></v-img>
+ 
+           <v-card-text>
+            <h1 class="text--primary mb-3">Lorem
+               Ipsum</h1>
+             <p>Lorem Ipsum has been the industry's unknown printer took a galley of type and scrambled it to make a type
+               specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+               remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+               containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker
+               including versions of Lorem Ipsum.</p>
+           </v-card-text>
+ 
+           <v-card-actions>
+             <v-spacer></v-spacer>
+             <v-btn text class="warning">Edit</v-btn>
+             <v-btn class="success">Buy</v-btn>
+           </v-card-actions>
+         </v-card>
+        </v-col>
+     </v-row>
+   </v-container>
+ </template>
+ 
+ <script>
+ export default {
+   data() {
+    return {
+     }
+   }
+ }
+ </script>
